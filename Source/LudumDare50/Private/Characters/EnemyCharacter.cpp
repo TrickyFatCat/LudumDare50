57622247272916818b59e8f165a95a67a72acae7
Alexander Getmansky <alex@getsky.games>
2022-04-02 19:34:58 +0300

    Add enemy ai controller

--- Source/LudumDare50/Private/Characters/EnemyCharacter.cpp
+++ Source/LudumDare50/Private/Characters/EnemyCharacter.cpp
@@ -2,4 +2,17 @@
 
 
 #include "Characters/EnemyCharacter.h"
+#include "Characters/EnemyController.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AEnemyCharacter::AEnemyCharacter()
+{
+	AutoPossessAI = EAutoPossessAI::Disabled;
+	AIControllerClass = AEnemyController::StaticClass();
+	
+    if (GetCharacterMovement()) {
+        bUseControllerRotationYaw = false;
+        GetCharacterMovement()->bUseControllerDesiredRotation = true;
+        GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+    }
+}
---
+++ Source/LudumDare50/Public/Components/EnemyPerceptionComponent.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2022. Made by Title Goose Team during LudumDare 50. All rights reserved. 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "EnemyPerceptionComponent.generated.h"
+
+
+UCLASS()
+class LUDUMDARE50_API UEnemyPerceptionComponent : public UAIPerceptionComponent
+{
+	GENERATED_BODY()
+	
+};
