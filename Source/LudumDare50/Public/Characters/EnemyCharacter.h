a9c899f373f7e7e0bff46dd9f57e7b8e3ee1a8cb
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-04-02 11:20:57 +0300

    Create a base enemy C++ class

---
+++ Source/LudumDare50/Public/Characters/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2022. Made by Title Goose Team during LudumDare 50. All rights reserved. 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE50_API AEnemyCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
