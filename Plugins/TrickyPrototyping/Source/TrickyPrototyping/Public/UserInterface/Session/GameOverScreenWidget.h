edeeea6a3e5266a4112046883f2d52e0b5a9f191
Alexander Getmansky <alex@getsky.games>
2022-04-04 21:11:22 +0300

    Ad stats

--- Plugins/TrickyPrototyping/Source/TrickyPrototyping/Public/UserInterface/Session/GameOverScreenWidget.h
+++ Plugins/TrickyPrototyping/Source/TrickyPrototyping/Public/UserInterface/Session/GameOverScreenWidget.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/VerticalBox.h"
+#include "Interfaces/IHttpRequest.h"
 #include "UserInterface/Session/BaseSessionScreenWidget.h"
 #include "GameOverScreenWidget.generated.h"
 
@@ -24,6 +26,12 @@ protected:
 	UPROPERTY(meta=(BindWidget))
 	UButton* Button_NextLevel = nullptr;
 	
+	UPROPERTY(meta=(BindWidget))
+	UVerticalBox* Stats = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UUserWidget> PlayerStatRowWidgetClass;
+	
 private:
 	virtual void OnTransitionScreenShowed() override;
 	
@@ -33,5 +41,12 @@ private:
 	void OpenNextLevel() const;
 
 	UFUNCTION(BlueprintPure, Category="UI")
-	float GetFinalTime() const;
+	float GetFinalTime();
+
+	void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess);
+	UFUNCTION(BlueprintCallable)
+	void ShowStat();
+
+	float Time = 0.0f;
+	
 };
