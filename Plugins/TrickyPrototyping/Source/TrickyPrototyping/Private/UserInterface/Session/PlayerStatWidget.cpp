8dd6b378f66d1627a3422514806a5cc2df8c40b7
Alexander Getmansky <alex@getsky.games>
2022-04-04 21:38:51 +0300

    Add widget

--- Plugins/TrickyPrototyping/Source/TrickyPrototyping/Private/UserInterface/Session/PlayerStatWidget.cpp
+++ Plugins/TrickyPrototyping/Source/TrickyPrototyping/Private/UserInterface/Session/PlayerStatWidget.cpp
@@ -3,10 +3,14 @@
 
 #include "UserInterface/Session/PlayerStatWidget.h"
 
+#include "Components/TextBlock.h"
+
 void UPlayerStatWidget::SetUsername(const FString& Text)
 {
+	PlayerNameTextBlock->SetText(FText::FromString(Text));
 }
 
 void UPlayerStatWidget::SetScore(const FString& Text)
 {
+	StatBlock->SetText(FText::FromString(Text));
 }
